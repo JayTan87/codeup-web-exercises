43f8dda4da0f13fc490ef40a39ebbdc453844aaa
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-06-05 11:25:26 -0500

    add the first test and solution for intro-to-testing

--- js/conditionalbonus.js
+++ js/conditionalbonus.js
@@ -1,68 +1,74 @@
 "use strict"
 
-// function favoriteDayWeek() {
-//     let userFavoriteDay = prompt("What is your favorite day of the week?");
-//     if(userFavoriteDay == "Monday" || "Tuesday" || "Wednesday" || "Thursday" || "Friday" || "Saturday" || "Sunday") {
-//         if(userFavoriteDay == "Monday") {
-//             alert("Monday message.");
-//         }
-//         else if(userFavoriteDay == "Tuesday") {
-//             alert("Tuesday message.");
-//         }
-//         else if(userFavoriteDay == "Wednesday") {
-//             alert("Wednesday message.");
-//         }
-//         else if(userFavoriteDay == "Thursday") {
-//             alert("Thursday message.");
-//         }
-//         else if(userFavoriteDay == "Friday") {
-//             alert("Friday message.");
-//         }
-//         else if(userFavoriteDay == "Saturday") {
-//             alert("Saturday message.");
-//         }
-//         else if(userFavoriteDay == "Sunday") {
-//             alert("Sunday message.");
-//         }
-//         else
-//             console.log("Input is invalid. Please try again.")
-//     }
-//
-// }
 function favoriteDayWeek() {
     let userFavoriteDay = prompt("What is your favorite day of the week?");
-    switch (userFavoriteDay.toLowerCase()) {
-        case "mon":
-        case "monday":
+    if(userFavoriteDay == "Monday" || "Tuesday" || "Wednesday" || "Thursday" || "Friday" || "Saturday" || "Sunday") {
+        if(userFavoriteDay == "Monday") {
             alert("Monday message.");
-            break;
-        case "tue":
-        case "tuesday":
+        }
+        else if(userFavoriteDay == "Tuesday") {
             alert("Tuesday message.");
-            break;
-        case "wed":
-        case "wednesday":
+        }
+        else if(userFavoriteDay == "Wednesday") {
             alert("Wednesday message.");
-            break;
-        case "thurs":
-        case "thursday":
+        }
+        else if(userFavoriteDay == "Thursday") {
             alert("Thursday message.");
-            break;
-        case "fri":
-        case "friday":
+        }
+        else if(userFavoriteDay == "Friday") {
             alert("Friday message.");
-            break;
-        case "sat":
-        case "saturday":
+        }
+        else if(userFavoriteDay == "Saturday") {
             alert("Saturday message.");
-            break;
-        case "sun":
-        case "sunday":
+        }
+        else if(userFavoriteDay == "Sunday") {
             alert("Sunday message.");
-            break;
-        default:
+        }
+        else
             alert("Input is invalid. Please try again.")
-            break;
     }
+
 }
+// function favoriteDayWeek() {
+//     let userFavoriteDay = prompt("What is your favorite day of the week?");
+//     switch (userFavoriteDay.toLowerCase()) {
+//         case "mon":
+//         case "monday":
+//             alert("Monday message.");
+//             break;
+//         case "tue":
+//         case "tuesday":
+//             alert("Tuesday message.");
+//             break;
+//         case "wed":
+//         case "wednesday":
+//             alert("Wednesday message.");
+//             break;
+//         case "thurs":
+//         case "thursday":
+//             alert("Thursday message.");
+//             break;
+//         case "fri":
+//         case "friday":
+//             alert("Friday message.");
+//             break;
+//         case "sat":
+//         case "saturday":
+//             alert("Saturday message.");
+//             break;
+//         case "sun":
+//         case "sunday":
+//             alert("Sunday message.");
+//             break;
+//         default:
+//             alert("Input is invalid. Please try again.")
+//             break;
+//     }
+// }
 favoriteDayWeek();
+let input = prompt("Please enter a number.");
+function numberOrNot(input) {
+    return typeof input === "number" ? true : false;
+
+}
+alert(numberOrNot(input));
