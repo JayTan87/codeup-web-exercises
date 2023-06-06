3397d92940ab53657375e84a60048d225fee748e
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-06-05 19:02:07 -0500

    added while.js and completed break_and_continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,31 +1,16 @@
-let userNumber = prompt("Please enter an odd number between 1 and 50.");
+let userNumber
 do {
-    if(userNumber <= 50 && userNumber >=1) {
-
-        for (let i = 1; i <= 50; i += 2) {
-            console.log(`Here is an odd number: ${i}`);
-        }
+    userNumber = prompt("Please enter an odd number between 1-50?");
+    if(userNumber <= 0 || userNumber >= 51 || userNumber % 2 === 0) {
+        alert("Number entered is outside the acceptable range or an even number. Please try again.");
     }
-}
-while (userNumber <= 0 || userNumber >= 51);
-if(userNumber <= 50 && userNumber >=1) {
-
-    for (let i = 1; i <= 50; i += 2) {
-        console.log(`Here is an odd number: ${i}`);
+    else
+        break;
+} while (userNumber <= 0 || userNumber >= 51 || userNumber % 2 === 0);
+for (let i = 1; i <= 50; i += 2) {
+    if(i == userNumber) {
+        console.log(`Yikes! Skipping over: ${userNumber}`);
+        continue;
     }
+    console.log(`There is an odd number: ${i}.`);
 }
-    else {
-    // do {
-    //     alert(`Input is invalid. please try again`)
-    //     userNumber = prompt("Please enter an odd number between 1 and 50.");
-    // }
-    // while (userNumber <= 0 || userNumber >= 51);
-
-}
-//     while(typeof userNumber !== "number") {
-//         alert("Invalid input. Please enter a number.")
-//      }
-// }
-// while(userNumber <= 0 || userNumber >= 51) {
-//     alert("Number is outside expected range.");
-// }
---
+++ js/while.js
@@ -0,0 +1,8 @@
+let num = 1;
+let i = 1;
+while(i <= 16) {
+    num = num * 2;
+    console.log(num);
+    ++i;
+}
+
