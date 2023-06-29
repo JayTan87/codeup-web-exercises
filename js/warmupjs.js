c5ca2bd90e10f236315bfc0ef02f1bff79a651eb
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-06-29 13:46:35 -0500

    updated files added defuse-the-bom.html

--- js/warmupjs.js
+++ js/warmupjs.js
@@ -55,45 +55,44 @@
 //
 // 	Example:
 
-
-const users = [
-
-	{
-		username: 'fred123',
-		password: 'letmein',
-		email: 'fred123@email.com',
-		isAdmin: true
-	},
-	{
-		username: 'cindy123',
-		password: 'hello123',
-		email: 'cindy123@email.com',
-		isAdmin: false
-	},
-	{
-		username: 'kathy123',
-		password: 'letmein',
-		email: 'kathy123@email.com',
-		isAdmin: true
-	},
-	{
-		username: 'kyle123',
-		password: 'letmein',
-		email: 'kyle123@email.com',
-		isAdmin: false
-	}
-]
-
-function returnAdmins(users) {
-let admins = [];
-	for(let i = 0; i < users.length; i += 1) {
-		if(users[i].isAdmin === true) {
-		admins.push(users[i].username);
-		}
-	}
-	return admins;
-}
-console.log(returnAdmins(users));
+// const users = [
+//
+// 	{
+// 		username: 'fred123',
+// 		password: 'letmein',
+// 		email: 'fred123@email.com',
+// 		isAdmin: true
+// 	},
+// 	{
+// 		username: 'cindy123',
+// 		password: 'hello123',
+// 		email: 'cindy123@email.com',
+// 		isAdmin: false
+// 	},
+// 	{
+// 		username: 'kathy123',
+// 		password: 'letmein',
+// 		email: 'kathy123@email.com',
+// 		isAdmin: true
+// 	},
+// 	{
+// 		username: 'kyle123',
+// 		password: 'letmein',
+// 		email: 'kyle123@email.com',
+// 		isAdmin: false
+// 	}
+// ]
+//
+// function returnAdmins(users) {
+// let admins = [];
+// 	for(let i = 0; i < users.length; i += 1) {
+// 		if(users[i].isAdmin === true) {
+// 		admins.push(users[i].username);
+// 		}
+// 	}
+// 	return admins;
+// }
+// console.log(returnAdmins(users));
 /*
 
 returnAdmins(users) returns...
@@ -115,3 +114,53 @@ returnAdmins(users) returns...
 ]
 
 */
+// Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable.
+// 	If the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
+// The function should return true if the neighborhood object is desireable and false otherwise.
+
+// example data...
+//
+// 	const neighborhood1 = {
+// 	neighborhood: "Lovely Estates",
+// 	medianHomePrice: 280000,
+// 	pool: true,
+// 	tennis: false,
+// 	crimeRate: "low",
+// 	schools: 22
+// };
+//
+// const neighborhood2 = {
+// 	neighborhood: "Luminous Estates",
+// 	medianHomePrice: 270000,
+// 	pool: true,
+// 	tennis: false,
+// 	crimeRate: "high",
+// 	schools: 24
+// }
+//
+// const neighborhood3 = {
+// 	neighborhood: "Oak Mountain",
+// 	medianHomePrice: 290000,
+// 	pool: false,
+// 	tennis: false,
+// 	crimeRate: "low",
+// 	schools: 24
+// }
+//
+// const neighborhood4 = {
+// 	neighborhood: "Ginormous Acres",
+// 	medianHomePrice: 350000,
+// 	pool: true,
+// 	tennis: true,
+// 	crimeRate: "low",
+// 	schools: 27
+// }
+//
+// checkNeighborhood = (neighborhood) => {
+// 	return ((neighborhood.medianHomePrice < 300000) && (neighborhood.crimeRate === "low") &&(neighborhood.schools >= 24)) ? true : false;
+//
+// }
+// console.log(checkNeighborhood(neighborhood1),"false");
+// console.log(checkNeighborhood(neighborhood2),"false");
+// console.log(checkNeighborhood(neighborhood3), "true");
+// console.log(checkNeighborhood(neighborhood4), "false");
