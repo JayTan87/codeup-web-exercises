f2bb0ff0da0c12279f087348aee6782fe8b76421
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-07-05 08:49:45 -0500

    pushed up old work

--- js/warmupjs.js
+++ js/warmupjs.js
@@ -164,3 +164,219 @@ returnAdmins(users) returns...
 // console.log(checkNeighborhood(neighborhood2),"false");
 // console.log(checkNeighborhood(neighborhood3), "true");
 // console.log(checkNeighborhood(neighborhood4), "false");
+// "use strict";
+//
+// // ========== Retrieve elements by id, class, tag name
+//
+// // id
+//     const header = document.getElementById('main-title');
+//     console.log(header);
+//
+// // class - will return an HTMLCollection
+//     const cards = document.getElementsByClassName('card');
+//     console.log(cards);
+//     console.log(cards[0]);
+//     // cards.pop(); // ERR/**/OR: Array specific method
+//     const cardsArr = Array.from(cards); // convert HTMLCollection and NodeList to Array
+//     cardsArr.pop();
+//     console.log(cardsArr);
+//
+//
+//
+// // tag
+//     const sections = document.getElementsByTagName('section');
+//     console.log(sections);
+//
+//
+// // ========== Query Selector and Query Selector All
+//
+// querySelector()
+//
+// const headerTitle = document.querySelector('header h1');
+// const headerTitle = document.querySelector('#main-title');
+// console.log(headerTitle);
+//
+// const li = document.querySelector('li'); // only returns first matching
+// console.log(li);
+//
+// const lis = document.querySelectorAll('li'); // only returns first matching
+// console.log(lis);
+//
+// const secondLi = document.querySelector('li:nth-child(2)');
+// console.log(secondLi);
+//
+//
+//
+// // ========== Direct access to form inputs
+//
+//     const forms = document.forms;
+//     console.log(forms['feedback-form'].feedback.value);
+//
+//
+// // ========== Accessing and modifying elements and properties
+//
+// // get value of innerHTML
+//     const heading = document.getElementById("main-heading");
+//     console.log(heading);
+//
+//     console.log(heading.innerHTML);
+//     console.log(heading.innerText);
+//
+// // set value of innerHTML
+//     heading.innerHTML = "<em>Hi MOM!</em>";
+//
+// // set value of innerText
+//     heading.innerText = "<em>Hi MOM!</em>";
+//
+// // append value to innerText (works the same with innerHTML)
+//     heading.innerHTML += " ...and hi Mom!";
+//
+//
+// // ========== Accessing and modifying attributes
+//
+// // check if attribute exists
+//     const cowboy = document.getElementById('cowboy');
+//     console.log(cowboy.hasAttribute('data-1'));
+//
+// // get an attribute value
+//     console.log(cowboy.getAttribute('data-1'));
+//
+// // create a new attribute or change a value of an existing attribute
+//     cowboy.setAttribute('data-1', 'hello');
+//     cowboy.setAttribute('data-test', 'testing');
+//     console.log(cowboy.getAttribute('data-1'));
+//
+// // remove attribute
+//     cowboy.removeAttribute("data-test");
+//     console.log(cowboy);
+//
+//
+// // ========== Accessing and modifying styles
+//
+// // single style
+//     const jumbotron = document.querySelector('.jumbotron');
+//     jumbotron.style.display = "none";
+//     jumbotron.style['font-family'] = "times";
+//
+// // apply multiple style changes
+//     Object.assign(jumbotron.style, {
+//         border: "10px solid black","font-family": "times", "text-decoration": "underline"
+//     });
+//
+// // styling node list
+//     const tableRows = document.getElementsByTagName("tr");
+//     for (let i = 0; i < tableRows.length; i += 1) {
+//         tableRows[i].style.background = "red";
+//     }
+//
+//
+// // ========== Add / Remove Elements
+//
+// // see curriculum links
+
+// Write a function, returnLargestStudentCount, that takes in an array of classes returns the number of students in the largest class.
+// Assume at least one class object will be present in the input array with students property set to a valid positive integer.
+
+// let classes = [
+// 	{class: "6th grade history", students: 18},
+// 	{class: "7th grade history", students: 20},
+// 	{class: "8th grade history", students: 22},
+// 	{class: "4th grade history", students: 30},
+// 	{class: "10th grade history", students: 25},
+//
+// ];
+//
+// returnLargestStudentCount = () => {
+// 	let largestStudentCount;
+// 	let classIds = classes.map(elements => {
+// 		return elements.students;
+// 	});
+// 	console.log(Math.max(...classIds));
+//
+// }
+//
+//
+// returnLargestStudentCount(classes); // returns 30
+"use strict";
+
+// https://developer.mozilla.org/en-US/docs/Web/Events
+
+// ==========================  Simple Example
+
+// When a user clicks the test button, log "test" in the console. (with pre-built function or anon function)
+const testBtn = document.getElementById('test-btn');
+// testBtn.addEventListener('mouseover', () =>  {
+// 	console.log('test');
+// });
+
+		function logTest() {
+			console.log('test');
+		}
+ testBtn.addEventListener('click', logTest)
+// remove event listener
+
+// testBtn.removeEventListener("click", logTest);
+
+
+
+// ==========================  Register Additional Events
+
+/* When a cursor hovers over the h1, change the text color, font-family, and make larger.
+
+    1.  target the node
+    2.  add event listener for mouseover event to target
+    3.  in listener change styling for paragraph
+
+ */
+let changeStyle =() => {
+	h1.style.color = "red";
+	h1.style.fontFamily = "fantasy";
+	h1.style.fontSize = "10em";
+}
+
+const h1 =document.getElementsByTagName('h1')[0];
+h1.addEventListener('mouseover', changeStyle);
+// When double-clicking the restore text button, make the h1 font size 2em
+const textBtn=document.getElementById('shrink-btn');
+
+textBtn.addEventListener('dblclick', () => {
+	h1.style.fontSize='2em';
+})
+
+// ==========================  Event Object
+
+// document.addEventListener("click", (e) => {
+//    console.log(e);
+// });
+
+
+// document.addEventListener("click", (e) => {
+//
+//    let red = e.screenX % 256;
+//    let green = e.screenY % 256;
+//    let blue = "ff";
+//
+//    console.log(e.screenX);
+//    console.log(e.screenY);
+//
+//    red = red.toString(16);
+//    green = green.toString(16);
+//
+//    document.body.style['background-color'] = "#" + red + green + blue;
+//
+// });
+
+// ==========================  Keyboard Events and Prevent Default
+
+// document.addEventListener("keypress", (e) => {
+//     console.log(e.code);
+//     if (e.code === "Space") {
+//         alert('HELLO!');
+//         // e.preventDefault();
+//     }
+// });
+
+
+// ==========================  e.target
+
+// *** see additional example
